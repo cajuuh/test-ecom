7a10b5a87462ba4553b6ccba6f1cc4f7cc15c099
Pedro Alcântara <pedro.filipi.alcantara@gmail.com>
2017-02-07 21:35:35 -0300

    update with gustavo

--- js/script.js
+++ js/script.js
@@ -66,6 +66,15 @@ $(document).ready(function() {
 		}
 	});
 
+	//Navbar
+	function myFunction() {
+		var x = document.getElementById("myTopnav");
+		if (x.className === "topnav") {
+			x.className += " responsive";
+		} else {
+			x.className = "topnav";
+		}
+	}
 
 	//Google Map
     var get_latitude = $('#google-map').data('latitude');
